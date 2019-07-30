872a122081bf9ec7bfeb777f4956bf0f20a8af8b
Jeanluc Kabulu <jeanluc.kabulu@gmail.com>
2019-07-31 01:39:16 +0200

    Compiling Pakcages

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "django-matchbox-orm",
+    version = "0.1",
+    scripts = ['django-matchbox-orm'],
+    author = "Jean-Luc Kabulu",
+    author_email = "jeanluc.kabulu@gmail.com",
+    description = "django-matchbox-orm is orm package for Google Cloud firestore with Django",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python ::3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['matchbox-orm>=0.2'],
+)
